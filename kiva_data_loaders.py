06166ebdb8f2136040908f406416d4bc4fcd6c3b
Richard Decal <richard.decal@ncf.edu>
2018-03-06 20:02:43 -0500

    completed helper funcs

---
+++ kiva_data_loaders.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+
+def load_all():
+    loans_data = load_loan_data()
+    mpi_location_data = load_locations_data()
+    loan_theme_ids_data = load_theme_id_data()
+    loan_themes_by_region_data = load_loan_themes_by_region_data()
+    return loans_data, mpi_location_data, loan_theme_ids_data, loan_themes_by_region_data
+
+
+def load_loan_data():
+    return pd.read_csv("data/kiva_loans.csv")
+
+
+def load_locations_data():
+    return pd.read_csv("data/kiva_mpi_region_locations.csv")
+
+
+def load_theme_id_data():
+    return pd.read_csv("data/loan_theme_ids.csv")
+
+
+def load_loan_themes_by_region_data():
+    return pd.read_csv("data/loan_themes_by_region.csv")
+
+
