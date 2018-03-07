fd672463cb09b8e29224197bf50010a4c6e60b8d
Richard Decal <richard.decal@ncf.edu>
2018-03-06 21:01:27 -0500

    added many comments, other improvements (see commit details)
    cleaned imports
    cleaned data loading
    customized colors
    improved plot labels
    deleted vestigial code

--- kiva_cloropleth.py
+++ kiva_cloropleth.py
@@ -11,69 +11,46 @@ Dataset:
 https://www.kaggle.com/kiva/data-science-for-good-kiva-crowdfunding
 '''
 
-import plotly.plotly as py
 import plotly
-import pandas as pd
 from kiva_data_loaders import *
 
-loans_data, mpi_location_data, loan_theme_ids_data, loan_themes_by_region_data = load_all()
+# use data load helper I made in kiva_data_loaders.py
+loans_data = load_loan_data()
 
 # view loans data with:
 # loans_data.head()
 
 # code adapted from https://www.kaggle.com/codename007/a-very-extensive-kiva-exploratory-analysis
-countries_funded_amount = loans_data.groupby('country').mean()['funded_amount'].sort_values(ascending = False)
+countries_funded_amount = loans_data.groupby('country').mean()[
+    'funded_amount']\
+
+# If you need to sort that data, you can use the .sort_values(ascending=False)
+# method
 
 data = [dict(
-        type='choropleth',
-        locations= countries_funded_amount.index,
-        locationmode='country names',
-        z=countries_funded_amount.values,
-        text=countries_funded_amount.index,
-        colorscale='Red',
-        marker=dict(line=dict(width=0.7)),
-        colorbar=dict(autotick=False, tickprefix='', title='Top Countries with funded_amount(Mean value)'),
+    type='choropleth',
+    locations=countries_funded_amount.index,  # list of country names
+    locationmode='country names',
+    # values used to color in countries
+    z=countries_funded_amount.values,  # ...and their associated value means
+    # text=countries_funded_amount.index,
+    # other colorscales are available here:
+    # https://plot.ly/ipython-notebooks/color-scales/
+    colorscale='Greens',
+    marker={'line': {'width': 0.5}},  # width of country boundaries
+    colorbar={'autotick': False, 'tickprefix': '$',
+              'title': 'Mean funded amount<br>in USD'},  # colorbar title
 )]
-layout = dict(title = 'Top Countries with funded_amount(Dollar value of loan funded on Kiva.org)',)
-fig = dict(data=data, layout=layout)
+layout = dict(
+    title = 'Mean Kiva Microloan Size<br>Source:\
+            <a href="https://www.kaggle.com/kiva/data-science-for-good-kiva-crowdfunding"">\
+            Kaggle</a>',
+    geo ={'showframe': False}  # hide frame around map
+    )
+fig = {'data': data, 'layout': layout}
+
+# switch to offline plots to avoid runtime exception
+# from not having Plotly account
+# ref https://plot.ly/python/getting-started/#initialization-for-offline-plotting
 plotly.offline.plot(fig, validate=False, filename='d3-world-map.html')
 
-
-# data = [ dict(
-#         type = 'choropleth',
-#         locations = df['CODE'],
-#         z = df['GDP (BILLIONS)'],
-#         text = df['COUNTRY'],
-#         colorscale = [[0,"rgb(5, 10, 172)"],[0.35,"rgb(40, 60, 190)"],[0.5,"rgb(70, 100, 245)"],\
-#             [0.6,"rgb(90, 120, 245)"],[0.7,"rgb(106, 137, 247)"],[1,"rgb(220, 220, 220)"]],
-#         autocolorscale = False,
-#         reversescale = True,
-#         marker = dict(
-#             line = dict (
-#                 color = 'rgb(180,180,180)',
-#                 width = 0.5
-#             ) ),
-#         colorbar = dict(
-#             autotick = False,
-#             tickprefix = '$',
-#             title = 'GDP<br>Billions US$'),
-#       ) ]
-#
-# layout = dict(
-#     title = '2014 Global GDP<br>Source:\
-#             <a href="https://www.cia.gov/library/publications/the-world-factbook/fields/2195.html">\
-#             CIA World Factbook</a>',
-#     geo = dict(
-#         showframe = False,
-#         showcoastlines = False,
-#         projection = dict(
-#             type = 'Mercator'
-#         )
-#     )
-# )
-#
-# fig = dict(data=data, layout=layout)
-#
-# # switch to offline plots
-# # ref https://plot.ly/python/getting-started/#initialization-for-offline-plotting
-# plotly.offline.plot(fig, validate=False, filename='d3-world-map.html')
