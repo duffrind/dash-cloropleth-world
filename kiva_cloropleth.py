8bc4a0183e78ac44afc61271b825039f47da09e1
Richard Decal <richard.decal@ncf.edu>
2018-03-06 21:33:43 -0500

    switched to num loans, added comments, added references

--- kiva_cloropleth.py
+++ kiva_cloropleth.py
@@ -9,6 +9,9 @@ https://www.kaggle.com/codename007/a-very-extensive-kiva-exploratory-analysis
 
 Dataset:
 https://www.kaggle.com/kiva/data-science-for-good-kiva-crowdfunding
+
+See more cloropleth options:
+https://plot.ly/python/reference/#choropleth
 '''
 
 import plotly
@@ -21,8 +24,7 @@ loans_data = load_loan_data()
 # loans_data.head()
 
 # code adapted from https://www.kaggle.com/codename007/a-very-extensive-kiva-exploratory-analysis
-countries_funded_amount = loans_data.groupby('country').mean()[
-    'funded_amount']\
+countries_funded_amount = loans_data.groupby('country').size()
 
 # If you need to sort that data, you can use the .sort_values(ascending=False)
 # method
@@ -30,15 +32,24 @@ countries_funded_amount = loans_data.groupby('country').mean()[
 data = [dict(
     type='choropleth',
     locations=countries_funded_amount.index,  # list of country names
+    # other option is USA-states
     locationmode='country names',
-    # values used to color in countries
+    # sets the color values
     z=countries_funded_amount.values,  # ...and their associated value means
+    # sets the text element associated w each position
     # text=countries_funded_amount.index,
     # other colorscales are available here:
     # https://plot.ly/ipython-notebooks/color-scales/
     colorscale='Greens',
+    # by default, low numbers are dark and high numbers are white
+    reversescale = True,
+    # set upper bound of color domain (see also zmin)
+    zmax=30000,
+    # if you want to use zmin or zmax don't forget to disable zauto
+    zauto=False,
     marker={'line': {'width': 0.5}},  # width of country boundaries
-    colorbar={'autotick': False, 'tickprefix': '$',
+    colorbar={'autotick': True,
+              'tickprefix': '',  # could be useful if plotting $ values
               'title': 'Mean funded amount<br>in USD'},  # colorbar title
 )]
 layout = dict(
@@ -51,6 +62,6 @@ fig = {'data': data, 'layout': layout}
 
 # switch to offline plots to avoid runtime exception
 # from not having Plotly account
-# ref https://plot.ly/python/getting-started/#initialization-for-offline-plotting
+# see https://plot.ly/python/getting-started/#initialization-for-offline-plotting
 plotly.offline.plot(fig, validate=False, filename='d3-world-map.html')
 
